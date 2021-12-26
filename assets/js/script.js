f68f54ee8b2bf9e3e74e11a86db19071be14a846
Romario <morrisromo9966@gmail.com>
2021-12-26 17:45:56 -0500

    trying different variatons to get colorchange working

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,54 @@ var userText9 = document.querySelector("#user-text-17");
 var userSaveBtn9 = document.querySelector("#user-save-Btn-17");
 var currentDate = moment().format("MMM Do YYYY, h:mm:ss a");
 
+
 // displays current date
 var currentDateDisplay = function () {
     document.getElementById("currentDay").innerText = currentDate;
 }
 currentDateDisplay();
+
+// // list
+// $(".list-group").on("click", "textarea", function () {
+//     console.log(this);
+// });
+
+
+
+// have the function execute when the DOM is loaded
+$(document).ready(function () {
+    changeColor();
+    //   saveText();
+});
+
+var changeColor = function () {
+    //have the color change depending on the Current time 
+    var currentTime = moment().hours();
+    // if the current time is eagual to true OR fals then put in the console.
+    if (currentTime === "true") {
+        console.log("This is current time")
+    } else (
+        console.log("this is NOT the current time")
+    )
+
+    $("user-text").each(function () {
+        var checkTime = parseInt($(this).attr("class"));
+        console.log(checkTime);
+
+        if (currentTime > checkTime) {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        } else (currentTime < checkTime); {
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            $(this).addClass("pass");
+        } else {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+    })
+};
 
 
